8c8960835be5302e0b320406725751ac8a599be6
velthereal <lera2005olex@gmail.com>
2023-09-25 13:34:25 +0300

    finished color picker and size picker

--- js/script.js
+++ js/script.js
@@ -1,46 +1,36 @@
 let canvas = document.getElementById('canva');
+let color_cells = document.querySelectorAll('.colors li');
 let ctx = canvas.getContext('2d');
+let penSize = document.querySelector('input[type="range"]');
+let size = 50 / 5;
+let colorPicker = document.querySelector('input[type="color"]');
+let currentColor = 'black';
 
+penSize.addEventListener('input', () => {
+	size = penSize.value / 5;
+	console.log(penSize.value);
+})
+
+colorPicker.addEventListener('input', () => {
+	currentColor = colorPicker.value;
+})
+
+for(let i = 0; i < color_cells.length; i++){
+	color_cells[i].addEventListener('click', function(e){
+		let colorClick = e.target.getAttribute('data-color');
+		currentColor = colorClick;
+	});
+}
 canvas.addEventListener('mousedown', function(){
 	canvas.onmousemove = function(e){
 		let x = e.offsetX;
 		let y = e.offsetY;
 		ctx.beginPath();
-		ctx.arc(x,y,5,0, Math.PI * 2);
-		ctx.fillStyle = 'black';
-		// ctx.rect(x, y, 5, 5,);
+		ctx.arc(x,y,size,0, Math.PI * 2);
+		ctx.fillStyle = currentColor;
 		ctx.fill();
-		canvas.style.cursor = 'pointer';
 	}
 	canvas.onmouseup = function(){
 		canvas.onmousemove = null;
-		canvas.style.cursor = 'default';
-
 	}
-})
-
-// canvas.onmousedown = function(e){
-// 	canvas.onmousemove = function(e){
-// 		let x = e.offsetX;
-// 		let y = e.offsetY;
-// 		ctx.fillRect(x, y, 2, 2);
-// 		ctx.fillStyle = 'red';
-// 		ctx.fill();
-// 	}
-// 	canvas.onmouseup = function(){
-// 		canvas.onmousemove = null;
-// 	}
-// }
-
-
-
-
-// let color_cells = document.querySelectorAll('.colors li');
-
-// for(let i = 0; i < color_cells.length; i++){
-// 	color_cells[i].addEventListener('click', changeColor);
-// }
-// function changeColor(e){
-// 	let colorClick = e.target.getAttribute('data-color');
-// 	// text.style.color = colorClick;
-// }
+});
